7b453669fb5e8814decde65700f1a88b4c64cfa2
chinh <bjergsen999@Gmail.com>
2023-03-10 16:36:27 +0700

    update-commit

--- src/main/java/Java_day7/Collection/Queue/Example.java
+++ src/main/java/Java_day7/Collection/Queue/Example.java
@@ -1,4 +1,26 @@
 package Java_day7.Collection.Queue;
 
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Example {
+    public static void main(String[] args) {
+        Queue<Message> queue = new ArrayDeque<>();
+        queue.add(new Message(1,"chinh"));
+        queue.add(new Message(2,"thủy"));
+        queue.add(new Message(3,"Kien"));
+        queue.add(new Message(4,"Thang"));
+        queue.add(new Message(5,"TRANg"));
+
+        while (!queue.isEmpty()){
+            Message msg = queue.poll();
+            System.out.println(msg);
+            try{
+                Thread.sleep(3000);
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }
+    }
 }
